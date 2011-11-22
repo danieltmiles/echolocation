dae44a86f412547cbca48a0f577a06eb469f6df9
Daniel Miles <daniel.t.miles@gmail.com>
2011-11-21 17:38:19 -0800

    a little more clean up that should have gone in the last commit

--- EchoLocation/src/com/amateurbikenerd/echoLocation/EchoLocationActivity.java
+++ EchoLocation/src/com/amateurbikenerd/echoLocation/EchoLocationActivity.java
@@ -1,22 +1,28 @@
 package com.amateurbikenerd.echoLocation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
 public class EchoLocationActivity extends Activity {
+	private Intent svc;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Intent svc = new Intent(this, NoiseService.class);
+		svc = new Intent(this, NoiseService.class);
 	    startService(svc);
 	}
 	@Override
 	public void onDestroy(){
-		Intent svc = new Intent(this, NoiseService.class);
+		//Intent svc = new Intent(this, NoiseService.class);
 	    stopService(svc);
 	    super.onDestroy();
 	}
